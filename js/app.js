13e580d8f764d4bc662733279aa284f94b48c352
Bhaskar Paul <paulbhaskar007@gmail.com>
2025-08-17 01:41:33 +0530

    update app.js

--- js/app.js
+++ js/app.js
@@ -1,23 +1,3 @@
-import Faker from "faker";
-
-for (let i = 0; i < 10; i++) {
-	console.log("Hello World");
-}
-
-for (let i = 10; i < 20; i++) {
-	console.log("Hello World");
-}
-
-for (let i = 20; i < 30; i++) {
-	console.log("Hello World");
-}
-
-console.log(
-	(function add(a, b) {
-		return a + b;
-	})(1, 2)
-);
-
-export const fetchDemandCreationTableColumns = () => {
-	return [];
+export const aNewFunction = () => {
+	return "aNewFunction"
 }
